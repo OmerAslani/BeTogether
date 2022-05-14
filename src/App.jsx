f6b58a9f7ef94fdc48a6a4f74e68564c0eb198ae
OmerAslani <aslaniomer21@gmail.com>
2022-05-14 11:54:11 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Component } from "react";
+
+// import Registar from './Components/Registar.jsx'
+
+
+function App() {
+    return ( 
+    <div className = "App" >
+            
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/Register.jsx
@@ -0,0 +1,38 @@
+import './Register.css';
+
+const Login = () => {
+    return(
+        
+        
+            <div class="wrapper fadeInDown">
+            <div id="formContent">
+            
+        
+            
+            <div class="fadeIn first">
+                <h2>BeTogether</h2>
+            </div>
+        
+            
+            <form class="form-log">
+                <input type="text" id="login" class="fadeIn second" name="name" placeholder="first name"></input>
+                <input type="text" id="text" class="fadeIn third" name="login" placeholder="Last name"></input>
+                <input type="text" id="email" class="fadeIn third" name="login" placeholder="Email"></input>
+                <input type="text" id="password" class="fadeIn third" name="login" placeholder="Password"></input>
+                <input type="text" id="password" class="fadeIn third" name="login" placeholder="Confirm Password"></input>
+                <input type="submit" class="fadeIn fourth" value="Create Account"></input>
+            
+            </form>
+        
+            
+            <div id="formFooter">
+                <a class="underlineHover" href="#">Log in</a>
+            </div>
+        
+            </div>
+        </div>
+      
+    )
+}
+
+export default Login
